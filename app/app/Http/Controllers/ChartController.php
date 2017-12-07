16b06346bce9e7cf66269392ce5f7308f5a9c88d
Kalada Kienka <kaladakienka@Kaladas-MacBook-Pro.local>
2017-12-07 21:51:04 +0100

    modified path to export file

--- app/app/Http/Controllers/ChartController.php
+++ app/app/Http/Controllers/ChartController.php
@@ -9,7 +9,7 @@ class ChartController extends Controller
     public function generatedata(Request $request)
     {
         $counter = 0;
-        $filePath = '/Users/kaladakienka/Documents/My_Projects/Temper/app/export.csv';
+        $filePath = __DIR__ . '/../../../' . 'export.csv';
 
         $userStepMap = array();
         foreach(file($filePath) as $line) {
